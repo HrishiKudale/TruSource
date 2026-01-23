4b7242fe66d78a29819e995b05a58c71cf370cef
HRISHIKESH KAMALAKAR KUDALE <2020.hrishikesh.kudale@ves.ac.in>
2026-01-23 17:47:40 +0530

    Update mongo.py

--- backend/mongo.py
+++ backend/mongo.py
@@ -1,9 +1,9 @@
-# backend/mongo.py
 from flask_pymongo import PyMongo
 
 mongo = PyMongo()
 
 def init_mongo(app):
-    app.config["MONGO_URI"] = app.config.get("MONGO_URI")
+    # Replace with your Atlas URI
+    app.config["MONGO_URI"] = "mongodb+srv://precisiontraceability_db_user:RqT4DWJAi3FeEXUd@traceability.c9crsed.mongodb.net/"
     mongo.init_app(app)
     return mongo
